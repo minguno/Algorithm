8ce3df0c3d90d8c456550c0365961cc51fe1c3d8
minguno <minguno1128@gmail.com>
2022-01-19 15:37:31 +0900

    자연수 N의 자릿수 합

--- SWEA/SWEA_2058_D1.py
+++ SWEA/SWEA_2058_D1.py
@@ -1,13 +1,33 @@
+# 처음 짠 코드
 # 입력받는 자연수 N
-N = input()
+# N = input()
+#
+# if len(N) == 1:
+#     print(f'{int(N)}')
+# elif len(N) == 2:
+#     print(f'{int(N[0]) + int(N[1])}')
+# elif len(N) == 3:
+#     print(f'{int(N[0]) + int(N[1]) + int(N[2])}')
+# elif len(N) == 4:
+#     print(f'{int(N[0]) + int(N[1]) + int(N[2]) + int(N[3])}')
+# else:
+#     pass
 
-if len(N) == 1:
-    print(f'{int(N)}')
-elif len(N) == 2:
-    print(f'{int(N[0]) + int(N[1])}')
-elif len(N) == 3:
-    print(f'{int(N[0]) + int(N[1]) + int(N[2])}')
-elif len(N) == 4:
-    print(f'{int(N[0]) + int(N[1]) + int(N[2]) + int(N[3])}')
-else:
-    pass
+# 더 쉬운 코드
+# 입력받은 자연수 N
+N = int(input())
+sum = 0
+
+# N은 최대 9999까지라 4자릿수
+for i in range(0, 4):
+    # 자연수가 아닐 때 실행하지 않는 제약
+    if N <= 0:
+        break;
+    # 1의 자리 j
+    j = N % 10
+    # 자릿수를 줄인 다음 N에 assign; 다음 반복에 10의 자리가 1의 자리가 됨
+    N = int(N / 10)
+    # 계속 만들어낸 1의 자릿수를 sum에 더하기
+    sum = sum + j
+
+print(sum)
