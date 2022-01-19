cf4d769ac914f41490fd25711424d34aee750970
minguno <minguno1128@gmail.com>
2022-01-19 10:48:13 +0900

    T만큼 반복되는 10개 정수 중 최댓값 출력 작업

---
+++ SWEA/SWEA_2068_D1.py
@@ -0,0 +1,8 @@
+# test case의 개수
+T = int(input())
+
+# 반복되는 횟수 겸 출력 번호 i
+for i in range(1, T + 1):
+    # 공백을 기준으로 구분하는 정수 리스트 생성
+    numbers = list(map(int, input().split()))
+    print('#{} {}'.format(i, max(numbers)))
