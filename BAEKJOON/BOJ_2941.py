e5f3169fde4d4743779b2cce16af0ecbf52a4701
minguno <minguno1128@gmail.com>
2022-01-23 03:24:12 +0900

    크로아티아 알파벳 .replace()로 문자열 길이 맞춰주기

--- BAEKJOON/BOJ_2941.py
+++ BAEKJOON/BOJ_2941.py
@@ -7,6 +7,22 @@ encrypted_word = input()
 
 special = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
+for char in special:
+    # 해당 문자열이 존재하면 길이가 1인 아무 임의의 string으로 대체하는
+    # .replace() 함수를 써 원래 알파벳과 길이수를 맞춰준다
+    encrypted_word = encrypted_word.replace(char, '_')
+
+print(len(encrypted_word))
+
+'''
+아래는 내가 처음 코드 짠 방식..
+
+# 입력받을 단어
+# 제공되지 않은 운영체제로 변경해서 사용한 크로아티아 알파벳 값
+encrypted_word = input()
+
+special = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
 # 원래 크로아티아 알파벳의 개수를 담을 변수
 original_word_length = len(encrypted_word)
 
@@ -19,8 +35,8 @@ for char in special:
 
 print(original_word_length)
 
-'''
-코드 설명
+
+=> 코드 설명
 
 'dz='의 경우 혼자만 1번 있을 때 마다 단어 길이가 -2씩 감소한다
 
