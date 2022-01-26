7b906cad859daf830b001317f7cf72f314b0e68e
minguno <minguno1128@gmail.com>
2022-01-26 12:41:07 +0900

    BOJ 9단계 큰 범위 내에 소수 출력하기 (에라토스테네스의 체)

--- BAEKJOON/BOJ_1929.py
+++ BAEKJOON/BOJ_1929.py
@@ -14,8 +14,8 @@ for i in range(M, N + 1):
 
 
 # 방법 2
-# 에라토스테네스의 체 원리로 배수를 False로 제외시켜 필요한 범위만큼 잘라서 출력
-# 시간 720ms로 상당히 빠름
+# 에라토스테네스의 체 원리로 2배수를 False로 제외시켜 필요한 범위만큼 잘라서 출력
+# 시간 소요가 상당히 적음
 
 
 import sys
@@ -32,9 +32,11 @@ is_prime[1] = False
 
 # 에라토스테네스의 체 원리를 사용해서 최대 범위를 제곱근 + 1 시켜 반복 횟수를 줄여준다
 for i in range(2, int(N ** 0.5) + 1):
-    # 2부터 그 배수를 False로 제외시켜준다
-    for j in range(i + i, N + 1, i):
-        is_prime[j] = False
+    # 생략해도 결과는 똑같이 출력되지만 시간 소요가 절반 감소한다 (720 -> 344ms)
+    if is_prime[i]:
+        # 2부터 그 배수를 False로 제외시켜준다
+        for j in range(i + i, N + 1, i):
+            is_prime[j] = False
 
 for i in list(idx for idx in range(M, N + 1) if is_prime[idx]):
     print(i)
