9079cee8073b0a6064c0c224b833404d6383b99b
minguno <minguno1128@gmail.com>
2022-01-26 12:36:31 +0900

    BOJ 8단계 큰 범위 내에 소수를 출력하기

--- BAEKJOON/BOJ_1929.py
+++ BAEKJOON/BOJ_1929.py
@@ -1,28 +1,40 @@
-M, N = map(int, input().split())
 
-sqrt_n = int(N ** 0.5)
+# 방법 1
+# all()로 소수 거르기
+# 시간 4896ms로 오래걸림
 
-# True 값이 제곱근 범위 내 수 만큼 생성된 리스트
-    # 에라토스테네스의 체 개념: 어떤 수의 약수는 그 수의 제곱근보다 크지 않다
-    # 에 근거하여 최대범위를 반절인 제곱근으로 줄인다
-# 이 리스트의 index를 수로 치환해서 쓸 것이다
+import sys
+
+M, N = map(int, sys.stdin.readline().split())
+
+for i in range(M, N + 1):
+    if i > 1 and all(i % x for x in range (2, int(i ** 0.5) + 1)):
+        print(i)
+
+
+
+# 방법 2
+# 에라토스테네스의 체 원리로 배수를 False로 제외시켜 필요한 범위만큼 잘라서 출력
+# 시간 720ms로 상당히 빠름
+
+
+import sys
+
+M, N = map(int, sys.stdin.readline().split())
+
+# 소수인지 구분할 리스트
+# 인덱스로 출력할 것이기 때문에 N + 1 해준다
 is_prime = [True] * (N + 1)
 
-# 0과 1은 소수가 아니기 때문에 제외해준다
+# 고로 0과 1은 제외시켜준다
 is_prime[0] = False
 is_prime[1] = False
 
-# 0, 1을 제외한 만큼 2부터 시작값을 잡고, 범위 + 1을 해서 (미만이라) 설정해준다
-for i in range(2, sqrt_n + 1):
-    # == True를 생략해도 된다. 이미 is_prime 자체가 boolean 으로 이루어진 리스트이기 때문.
-    if is_prime[i]:
-        # True == prime이라면, 그 수를 제외한 그 수의 다음 배수부터 (i + i) 지정한 범위(N)내에 모든 배수들을 (i씩 증감)
-        for j in range(i + i, N + 1, i):
-            # 제외해준다
-            is_prime[j] = False
+# 에라토스테네스의 체 원리를 사용해서 최대 범위를 제곱근 + 1 시켜 반복 횟수를 줄여준다
+for i in range(2, int(N ** 0.5) + 1):
+    # 2부터 그 배수를 False로 제외시켜준다
+    for j in range(i + i, N + 1, i):
+        is_prime[j] = False
 
-# 입력된 범위 (M, N + 1)로 자르고, boolean 리스트로 소수를 구분한 is_prime의 인덱스를 원소로 가져온다
-# if 문에 '== True' 또한 생략되었다
-prime_list = list(idx for idx in range(M, N + 1) if is_prime[idx])
-
-print(prime_list)
+for i in list(idx for idx in range(M, N + 1) if is_prime[idx]):
+    print(i)
