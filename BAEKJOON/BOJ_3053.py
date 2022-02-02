ac470fbbfc999203b36e2a7d8a8123b3adc79021
minguno <minguno1128@gmail.com>
2022-02-02 23:20:35 +0900

    BOJ 9단계 유클리드 기하학과 택시 기하학을 각각 적용한 원의 넓이 구하기

---
+++ BAEKJOON/BOJ_3053.py
@@ -0,0 +1,16 @@
+# 9단계 기본 수학2 택시 기하학
+# 유클리드 기하학의 원과 택시 기하학에서의 원에 대한 문제
+
+import math
+import sys
+
+r = sys.stdin.readline()
+print(math.pi * r * r)
+print(2 * r * r )
+
+'''
+택시 기하학에서의 원의 넓이는 밑변의 길이와 높이가 r인 삼각형 4개의 넓이의 합이다.
+
+4 * (1/2 * w * h) = 2 * r * r
+
+'''
