b02d9a56fbc1331ba6a26141a7f2b3400d973e43
minguno <minguno1128@gmail.com>
2022-02-06 01:29:38 +0900

    히스토그램 넓이 구하기 (창고 다각형)

--- BAEKJOON/BOJ_2304.py
+++ BAEKJOON/BOJ_2304.py
@@ -9,12 +9,12 @@ for _ in range(int(input())):
 points = sorted(points, key=lambda x: x[0])
 
 # x 인덱스에 y 저장
-# 인덱스 값이라 + 1 헤줘서 갯수 맞추기
+# 인덱스 값이라 + 1 해줘서 갯수 맞추기
 grid = [0] * (points[-1][0] + 1)
 maxx, maxy = 0, 0
 for idx, value in points:
     grid[idx] = value
-    # 최댓값이 1개 이상일 수 있으니 이하로 처리
+    # 최대높이 기둥이 1개 이상일 수 있으니 이하로 처리
     if maxy <= value:
         maxy = value
         maxx = idx
@@ -26,7 +26,7 @@ for i in range(maxx + 1):
         value = grid[i]
     total += value
 
-# y최댓값 이후
+# y최댓값 이후는 뒤집어서 똑같이
 value = 0
 for i in range(len(grid) - 1, maxx, -1):
     if grid[i] > value:
