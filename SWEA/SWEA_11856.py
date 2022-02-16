bc876b6aa32c66c6f3d868875afaf60527d0b3ab
minguno <minguno1128@gmail.com>
2022-02-16 21:24:15 +0900

    문자열 반반 검사

--- SWEA/SWEA_11856.py
+++ SWEA/SWEA_11856.py
@@ -1,5 +1,6 @@
 # 반반
 
+# 내가 한 방법 1
 for t in range(int(input())):
     word = input()
     l1 = []
@@ -22,3 +23,26 @@ for t in range(int(input())):
     if cnt < 4:
         sol = 'No'
     print(f'#{t + 1} {sol}')
+
+
+# 실행 시간이 더 빠른 방법 2
+for t in range(1, int(input()) + 1):
+    word = input()
+    temp = {}
+    for w in word:
+        if w in temp:
+            temp[w] += 1
+        else:
+            temp[w] = 1
+ 
+    vals = list(temp.values())
+    try:
+        if vals[0] == vals[1] == 2:
+            result = 'Yes'
+        else:
+            result ='No'
+    # 원소가 1개만 있을 때 vals[1]는 인덱스 에러
+    except IndexError:
+        result = 'No'
+         
+    print(f'#{t}', result)
