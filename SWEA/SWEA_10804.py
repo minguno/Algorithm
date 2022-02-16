b8fc1cadcf2e85647a054df9694c939c59a79e3d
minguno <minguno1128@gmail.com>
2022-02-16 17:28:14 +0900

    거울에 비춰진 문자열

--- SWEA/SWEA_10804.py
+++ SWEA/SWEA_10804.py
@@ -1,5 +1,6 @@
 # 문자열의 거울상
 
+# 방법 1
 for t in range(int(input())):
     p = input()
     q = p[::-1]
@@ -14,3 +15,13 @@ for t in range(int(input())):
         else:
             sol += 'p'
     print(f'#{t+1} {sol}')
+
+# 방법 2
+dct = {'b': 'd', 'd': 'b', 'p': 'q', 'q': 'p'}
+
+for t in range(int(input())):
+    word = input()
+    sol = ''
+    for i in range(len(word) - 1, -1, -1):
+        sol += dct[word[i]]
+    print(f'#{t + 1} {sol}')
