055d5919046710a700022da17329590383ef290a
minguno <minguno1128@gmail.com>
2022-04-03 22:40:09 +0900

    1-N 까지 중 M개의 수로 이루어진 가능한 모든 수열 출력 (중복, 비내림차순)

--- BAEKJOON/BOJ_15652.py
+++ BAEKJOON/BOJ_15652.py
@@ -1,16 +1,18 @@
 # N과 M (4)
 # [0]을 M개만큼 만들어 순열을 채워나가는 방식은 가지치기를 할 수 없어
 # 공백리스트에 차례대로 넣어주는 방식을 선택
+# 프린트 후 return을 써주지 않으면 런타임에러
 
 def perm(n, p):
     if p != sorted(p):
         return
     if n == M:
         print(*p)
+        return
     for i in range(N):
         perm(n + 1, p + [lst[i]])
 
 
 N, M = map(int, input().split())
-lst = [i + 1 for i in range(N)]
+lst = [i for i in range(1, N + 1)]
 perm(0, [])
